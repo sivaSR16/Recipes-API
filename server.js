654ba4bc050304ef3dfcd83e388309f05d938c75
sivaSR16 <sivaramoct7@gmail.com>
2025-10-25 18:34:04 +0530

    2m commit

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/recipes', recipeRoutes);  // 👈 this mounts your routes
+app.get("/", (req, res) => {
+  res.send("✅ Recipes API is live!");
+});
+
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
